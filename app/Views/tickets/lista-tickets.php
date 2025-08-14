004c086c682d109b0b8af9b4bd714b52c614a499
JavierTeheranM22 <javier@creamos.co>
2025-08-14 15:26:06 -0500

    lista de reportes actualizada

--- app/Views/tickets/lista-tickets.php
+++ app/Views/tickets/lista-tickets.php
@@ -46,17 +46,16 @@
                             <table class="datatable display table table-bordered">
                                 <thead>
                                     <tr>
-                                        <th class="text-center">#</th>
-                                        <th>ID</th>
-                                        <th>Título</th>
-                                        <th>Área</th>
-                                        <th>Cliente</th>
+                                        <th class="text-center"># consecutivo</th>
+                                        <th>Fecha de creación</th>
+                                        <th># Ticket</th>
+                                        <th>Categoría</th>
+                                        <th>Clasificación</th>
                                         <th>Prioridad</th>
-                                        <th>Estado</th>
-                                        <th>Creación</th>
-                                        <th>Vencimiento</th>
-                                        <th>(Horas)</th> <!-- Nuevo campo para mostrar el tiempo en horas -->
-                                        <th>TDR</th>
+                                        <th>Dependencia</th>
+                                        <th>Estatus</th>
+                                        <th>Operador</th>
+                                        <th>TDR (Horas)</th>
                                         <th class="text-center">Opciones</th>
                                     </tr>
                                 </thead>
@@ -66,11 +65,12 @@
                                         <?php foreach ($tickets as $ticket): ?>
                                             <tr>
                                                 <td class="text-center"><?= $contador; ?></td>
+                                                <td><?= date('d/m/Y H:i', strtotime($ticket['fecha_creacion'])) ?></td>
                                                 <td><strong><?= $ticket['identificador']; ?></strong></td>
-                                                <td><?= $ticket['titulo'] ?></td>
-                                                <td><?= $ticket['nombre_area'] ?></td>
-                                                <td><?= $ticket['nombre_cliente'] ?></td>
+                                                <td><?= $ticket['categoria'] ?? 'N/A' ?></td>
+                                                <td><?= $ticket['clasificacion'] ?? 'N/A' ?></td>
                                                 <td style="background-color: <?= htmlspecialchars($ticket['color_sla']) ?>;"><?= $ticket['prioridad'] ?></td>
+                                                <td><?= $ticket['dependencia'] ?? 'N/A' ?></td>
                                                 <td>
                                                     <?php
                                                     // Determinamos el color del estado
@@ -78,8 +78,7 @@
                                                     ?>
                                                     <span class="<?= $estado_clase; ?>"><?= $ticket['estado'] ?></span>
                                                 </td>
-                                                <td><?= date('d/m/Y H:i', strtotime($ticket['fecha_creacion'])) ?></td>
-                                                <td><?= date('d/m/Y H:i', strtotime($ticket['fecha_vencimiento'])) ?></td>
+                                                <td><?= $ticket['operador'] ?? 'N/A' ?></td>
                                                 <td>
                                                     <?php
                                                     // Calculamos la diferencia entre la fecha de creación y la fecha de vencimiento en horas
@@ -89,13 +88,6 @@
                                                     $diferencia_horas = round($diferencia_segundos / 3600); // Convertimos los segundos a horas
                                                     ?>
                                                     <?= $diferencia_horas ?> horas
-                                                </td>
-                                                <td>
-                                                    <?php
-                                                    $estado_sla = strtotime($ticket['fecha_vencimiento']) < time() ? 'Vencido' : 'Activo';
-                                                    $clase_sla = $estado_sla === 'Vencido' ? 'text-danger' : 'text-success';
-                                                    ?>
-                                                    <span class="<?= $clase_sla; ?>"><?= $estado_sla; ?></span>
                                                 </td>
                                                 <td class="text-center">
                                                     <a href="<?= base_url() . 'tickets/detalle/' . $ticket['id'] ?>" class="btn btn-info btn-sm">
